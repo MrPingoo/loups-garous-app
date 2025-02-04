a6451aa567d7f8e61cd043ab0b9e1974d0f2cc54
SaraTerrier <terriers616@gmail.com>
2025-02-04 15:20:53 +0100

    css page login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,15 +15,33 @@ function Login() {
 
     return (
         <div className="login-container">
-            <h1 className="login-title">Connexion</h1>
+            <h1 className="login-title">Loup-Garou</h1>
+            <p className="login-subtitle">Connectez-vous pour accéder à votre compte</p>
             <form onSubmit={handleSubmit} className="login-form">
-                    <input type="email" placeholder="Entrez votre email" required value={email}
-                           onChange={(e) => setEmail(e.target.value)}/>
-                    <input type="password" placeholder="Entrez votre mot de passe" required value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
-                    <button type="submit" className="login-button">Se connecter</button>
-                </form>
+                <label htmlFor="email" className="email-label">Email</label>
+                <input 
+                    type="email" 
+                    placeholder="Entrez votre email" 
+                    required 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    onFocus={(e) => e.target.placeholder = ""}
+                    onBlur={(e) => e.target.placeholder = "Entrez votre email"}
+                />
+                <label htmlFor="password" className="password-label">Mot de passe</label>
+                <input 
+                    type="password" 
+                    placeholder="Entrez votre mot de passe" 
+                    required 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    onFocus={(e) => e.target.placeholder = ""}
+                    onBlur={(e) => e.target.placeholder = "Entrez votre mot de passe"}
+                />
                 <a href="#" className="forgot-password">Mot de passe oublié ?</a>
+                <button type="submit" className="login-button">Connexion</button>
+                <a href="#" className="new-account">Vous n'avez pas de compte ?</a>
+            </form>
         </div>
     );
 }
